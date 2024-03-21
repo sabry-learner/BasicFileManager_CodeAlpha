f748ed364b087dffdf6b483d811c30b32f07d810
Ahmed Sabry <ms6934752@gmail.com>
2024-03-21 17:15:52 +0200

    project file is added

---
+++ BasicFileManager.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <codecvt>
+#include <locale>
+using namespace std;
+
+class FileManager {
+private:
+    wstring currentDirectory;
+
+    void listDirectoryContents(const wstring& path) {
+        WIN32_FIND_DATAW findFileData;
+        HANDLE hFind = FindFirstFileW((path + L"\\*").c_str(), &findFileData);
+        if (hFind != INVALID_HANDLE_VALUE) {
+            do {
+                wstring_convert<codecvt_utf8_utf16<wchar_t>> converter;
+                string fileName = converter.to_bytes(findFileData.cFileName);
+                cout << fileName << endl;
+            } while (FindNextFileW(hFind, &findFileData) != 0);
+            FindClose(hFind);
+        }
+    }
+
+    bool createDirectory(const wstring& path) {
+        return CreateDirectoryW(path.c_str(), NULL) != 0;
+    }
+
+    bool copyFile(const wstring& source, const wstring& destination) {
+        return CopyFileW(source.c_str(), destination.c_str(), FALSE) != 0;
+    }
+
+    bool moveFile(const wstring& source, const wstring& destination) {
+        return MoveFileW(source.c_str(), destination.c_str()) != 0;
+    }
+
+public:
+    FileManager() : currentDirectory(L".") {}
+
+    void run() {
+        string command;
+
+        while (true) {
+            cout << "\n\t\t========================" << endl;
+            cout<<  "\t\t|  Basic File Manager   |\n";
+            cout << "\t\t========================" << endl;
+            wcout << "\tCurrent Directory: " << currentDirectory << endl;
+            cout << "\tAvailable commands:" << endl;
+            cout << "\t1. List directory contents (ls)" << endl;
+            cout << "\t2. Create directory (mkdir)" << endl;
+            cout << "\t3. Copy file (cp)" << endl;
+            cout << "\t4. Move file (mv)" << endl;
+            cout << "\t5. Change directory (cd)" << endl;
+            cout << "\t6. Exit" << endl;
+            cout << "  =>Enter command number or its abbreviation: ";
+            cin >> command;
+
+            if (command == "ls" || command == "1") {
+                listDirectoryContents(currentDirectory);
+            }
+            else if (command == "mkdir" || command == "2") {
+                string newDirName;
+                cout << "\tEnter directory name: ";
+                cin >> newDirName;
+                if (createDirectory(currentDirectory + L"\\" + wstring(newDirName.begin(), newDirName.end())))
+                    cout << "\tDirectory created successfully :) ." << endl;
+                else
+                    cout << "\tFailed to create directory !!" << endl;
+            }
+            else if (command == "cp" || command == "3") {
+                string source, destination;
+                cout << "\tEnter source file path: ";
+                cin >> source;
+                cout << "\tEnter destination file path: ";
+                cin >> destination;
+                if (copyFile(wstring(source.begin(), source.end()), wstring(destination.begin(), destination.end())))
+                    cout << "\tFile copied successfully :) ." << endl;
+                else
+                    cout << "\tFailed to copy file !!" << endl;
+            }
+            else if (command == "mv" || command == "4") {
+                string source, destination;
+                cout << "\tEnter source file path: ";
+                cin >> source;
+                cout << "\tEnter destination file path: ";
+                cin >> destination;
+                if (moveFile(wstring(source.begin(), source.end()), wstring(destination.begin(), destination.end())))
+                    cout << "\tFile moved successfully :) ." << endl;
+                else
+                    cout << "\tFailed to move file !! " << endl;
+            }
+            else if (command == "cd" || command == "5") {
+                string newDir;
+                cout << "\tEnter directory path: ";
+                cin >> newDir;
+                currentDirectory = wstring(newDir.begin(), newDir.end());
+            }
+            else if (command == "exit" || command == "6") {
+                cout<<"\t\t==========================\n";
+                cout<<"\t\t Thanks To use My App  :) \n";
+                cout<<"\t\t==========================\n\n";
+                break;
+            }
+            else {
+                cout << "\tInvalid command, Please try again!!" << endl;
+            }
+
+            // Clear the screen after each command and wait for 2 seconds
+            Sleep(2000); // 2000 milliseconds (2 seconds)
+            system("cls"); // Clear the screen
+        }
+    }
+};
+
+int main() {
+    FileManager fileManager;
+    fileManager.run();
+
+    return 0;
+}
